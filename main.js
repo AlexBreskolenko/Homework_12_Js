6f90fab54064cae31b3a2ea36068e0dc38adf020
Alex <89929071+AlexBreskolenko@users.noreply.github.com>
2024-08-09 19:56:54 +0300

    Task completed

--- main.js
+++ main.js
@@ -10,14 +10,17 @@ async function getData(url) {
   }
 }
 
+const sectionOneCards = document.querySelector(".section-one__cards");
+const sectionTwoBasket = document.querySelector(".section-two");
+
 document.addEventListener("DOMContentLoaded", async (e) => {
   const data = await getData(url);
   data.forEach((element) => {
-    const sectonCards = document.querySelector(".section__cards");
-    sectonCards.insertAdjacentHTML(
+    sectionOneCards.insertAdjacentHTML(
       "beforeend",
       `<div class="cards">
           <img class="cards__img" src= ${element.img} />
+          <button id=${element.id} class="button__pop-up" href="#"><img id=${element.id}   class=""button__img src=${element.iconBacket} />${element.popUpText}</button>
           <div class="cards__content">
             <h3 class="cards-content__heading">${element.heading}</h3>
             <p class="cards-content__text">
@@ -27,5 +30,34 @@ document.addEventListener("DOMContentLoaded", async (e) => {
           </div>
         </div>`
     );
+  });
+  // Добавление при нажатии на кнопку
+  const btnEll = document.querySelectorAll(".button__pop-up");
+  btnEll.forEach((elem) => {
+    elem.addEventListener("click", (e) => {
+      sectionTwoBasket.insertAdjacentHTML(
+        "beforeend",
+        `<div class="section-two__basket"><img class="basket__img" src=${
+          data[e.target.id - 1].img
+        }><div class="basket__content"> <img class="basket__icon-close" src=./img/icon-close.svg> <h3 class="basket-content__heading">${
+          data[e.target.id - 1].heading
+        }</h3>
+            <p class="basket-content__text">
+              ${data[e.target.id - 1].text}
+            </p>
+            <span class="basket-content__price">${
+              data[e.target.id - 1].price
+            }</span></div></div>`
+      );
+    });
+  });
+  // Удоление с корзины
+  sectionTwoBasket.addEventListener("click", (e) => {
+    if (e.target.closest(".basket__icon-close")) {
+      const item = e.target.closest(".section-two__basket");
+      if (item) {
+        item.remove();
+      }
+    }
   });
 });
